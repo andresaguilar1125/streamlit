b947ce9012548b1058c2f6cb71f91b15a0c99f17
andresaguilar1125 <andresaguilar1125@gmail.com>
2025-10-22 19:12:43 -0600

    commit

--- app.py
+++ app.py
@@ -3,18 +3,9 @@ import pandas as pd
 import plotly.express as px
 import duckdb
 
-GOOGLESHEET_URL = "https://docs.google.com/spreadsheets/d/1rAkmgUkf8xcqlwdcnX0VpHAIgUrHgCRRhU8cBlsA3HA/export?format=csv&gid="  
-
-# Map categories to GIDs
-GID_CATEGORY = {
-    "Familiar": "164123414",
-    "Medico": "754260188",
-    "Recibos": "1534979300",
-    "Restaurantes": "1270398057",
-    "Super": "1192999871",
-    "Viajes": "856975249"
-}
-
+# ********************************** 
+# FUNCTIONS
+# **********************************
 @st.cache_data
 def get_sheet(gid, category_name):
     # Cached function to load Google Sheet as pandas DataFrame and add a category column
@@ -24,20 +15,52 @@ def get_sheet(gid, category_name):
     df["Monto"] = df["Monto"].str.replace(",", "").astype(float)
     return df
 
-# Concatenate all sheets and transform data
+def get_unique_values(df, column_name):
+
+    # Returns a list of unique, non-null values from the specified column in a pandas DataFrame.
+
+    # Parameters:
+    # - dataframe (pd.DataFrame): The DataFrame to process.
+    # - column_name (str): The name of the column to extract unique values from.
+
+    # Returns:
+    # - List of unique values.
+
+    return sorted(df[column_name].dropna().unique().tolist(), reverse=True)
+
+# ********************************** 
+# CONSTANTS
+# **********************************
+# Google Sheet Base URL
+GOOGLESHEET_URL = "https://docs.google.com/spreadsheets/d/1rAkmgUkf8xcqlwdcnX0VpHAIgUrHgCRRhU8cBlsA3HA/export?format=csv&gid="  
+
+# GID Google Sheets
+GID_CATEGORY = {
+    "Familiar": "164123414",
+    "Medico": "754260188",
+    "Recibos": "1534979300",
+    "Restaurantes": "1270398057",
+    "Super": "1192999871",
+    "Viajes": "856975249"
+}
+
+# Dataframes
 df = pd.concat([get_sheet(gid, category) for category, gid in GID_CATEGORY.items()])
 
-# st.write("### Combined & Transformed Data")
-# st.dataframe(df)
+# **********************************
+# WIDGETS
+# **********************************
+# Create multiselect widget with dynamic options
+persona = st.multiselect("Selecciona Persona", get_unique_values(df, "Persona"))
+
+# Filter DataFrame based on selected personas
+df = df[df["Persona"].isin(persona)]
+
+# ********************************** 
+# DISPLAY
+# **********************************
+st.write(df)
 
 
 # for dataframes tabs
-# st.write(duckdb.query(open("df_desktop.sql").read()) .to_df())
-# st.write(duckdb.query(open("df_mobile.sql").read()) .to_df())
 # st.write(duckdb.query(open("df_budget.sql").read()) .to_df())
-
-# for chart tabs
-df_extras = (duckdb.query(open("fig_extras.sql").read()) .to_df())
-df_extras
-fig = px.bar(df_extras, x="Periodo", y="Total", color="Categoria", barmode="stack")
-fig
